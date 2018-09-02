f907fb94e7f9cee1cdbdeddb9e110d296effdb60
nikhisah <sunilsah.sah414@gmail.com>
2018-09-02 17:19:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,125 @@
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+
+   
+
+  // Add task event
+  form.addEventListener('submit', addTask);
+
+  // removetask
+  taskList.addEventListener('click', removetask);
+
+ // clear task 
+ clearBtn.addEventListener('click', clearTask);
+
+ // fliter task 
+
+ filter.addEventListener('keyup' , filterTask)
+
+ 
+}
+
+
+
+
+
+
+// Add Task
+function addTask(e) {
+  if(taskInput.value === '') {
+    alert('Add a task');
+  }
+
+  // Create li element
+  const li = document.createElement('li');
+  // Add class
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement('a');
+  // Add class
+  link.className = 'delete-item secondary-content';
+  // Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append the link to li
+  li.appendChild(link);
+
+  // Append li to ul
+  taskList.appendChild(li);
+
+
+
+
+  // Clear input
+  taskInput.value = '';
+
+  e.preventDefault();
+}
+
+
+
+
+
+
+/*** remove the task  ***/
+
+function removetask(e){
+ if(e.target.parentElement.classList.contains('delete-item')) {
+  
+   if(confirm('are you Sure')) {
+    e.target.parentElement.parentElement.remove();
+
+   
+
+   }
+   
+
+ }
+   
+}
+
+
+
+
+
+/** clear task  */
+
+function clearTask() {
+   
+  taskList.innerHTML = " "
+
+
+}
+
+
+/** filter task **/
+
+function filterTask(e) {
+   
+  const text = e.target.value.toLowerCase();
+  
+  document.querySelectorAll('.collection-item').forEach(function(task){
+   const item = task.firstChild.textContent;
+   if(item.toLowerCase().indexOf(text) != -1 ){
+
+       task.style.display = 'block';
+   }
+
+   else {
+    task.style.display = 'none';
+   }
+
+  });
+
+}
